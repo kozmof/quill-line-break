4ed6bd560fbea0c1c8a964d9e24260cbae673acb
Kozo Oeda <kozo.oeda@gmail.com>
2025-03-18 18:03:33 +0900

    Format codes

--- src/modules/clipboard.ts
+++ src/modules/clipboard.ts
@@ -2,20 +2,26 @@ import Clipboard from 'quill/modules/clipboard';
 import Quill, { type Range } from 'quill';
 
 export class LineBreakClipboard extends Clipboard {
-  onPaste (range: Range, { text, html }: { text?: string;
-    html?: string; }): void {
+  onPaste(range: Range, { text, html }: {
+    text?: string;
+    html?: string;
+  }): void {
     if (typeof text == 'string') {
       text = text.replace(/\r\n/g, '\n').replace(/\r/g, '\n');
       if (text.endsWith('\n')) {
         text = text.slice(0, text.length - 1);
       }
 
-      super.onPaste(range, { text,
-        html });
+      super.onPaste(range, {
+        text,
+        html
+      });
       this.quill.setSelection(range.index + text.length, Quill.sources.USER);
     } else {
-      super.onPaste(range, { text,
-        html });
+      super.onPaste(range, {
+        text,
+        html
+      });
     }
   }
 }
