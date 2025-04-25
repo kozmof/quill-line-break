cbd4925c6c856608904732635ecb2085f2b5ce8f
Kozo Oeda <kozo.oeda@gmail.com>
2025-04-25 21:04:16 +0900

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export { LineBreak } from './blots/lineBreak';
 export { LineBreakClipboard } from './modules/clipboard';
 export { LineBreakScroll } from './blots/scroll';
-export { LineBreakCursor } from './blots/cursor';
 export { makeLineBreakEmbedArrowHandler } from './util/handler/makeEmbedArrowHandler';
 export { makeEnterHandler } from './util/handler/makeEnterHandler';
 export { setupLineBreak } from './util/setupLineBreak';
